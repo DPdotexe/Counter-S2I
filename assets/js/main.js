42d8c269d7e300c4b9e03eeef1331020bc7ed280
DPdotexe <danielepuleri97@gmail.com>
2023-07-31 19:22:36 +0200

    fix project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,47 @@
-let counter = 0;
+//creazione elementi
 
-function increase(){
-    counter +=1;
-    document.getElementById("number").innerHTML = counter;
-}
-function reset(){
-    counter=0
-    document.getElementById("number").innerHTML = counter;
-}
-function decrease(){;
-    counter -=1;
-    document.getElementById("number").innerHTML = counter;
-}
+let counterDisplay = document.createElement('span');
+counterDisplay.setAttribute("id","counterDP");
+let displayText = document.createTextNode("0")
+
+counterDisplay.appendChild(displayText)
+
+let addBtn = document.createElement("button");
+let addText = document.createTextNode("+");
+
+addBtn.appendChild(addText);
+
+let resetBtn = document.createElement("button");
+let resetText = document.createTextNode("Reset");
+
+resetBtn.appendChild(resetText);
+
+let subBtn = document.createElement("button");
+let subText = document.createTextNode("-");
+
+subBtn.appendChild(subText);
+
+// Insert in the DOM 
+
+let counter = document.getElementById('counter');
+counter.append(addBtn,resetBtn,subBtn,counterDisplay);
+
+// counter funzionalità
+let counterDP = document.getElementById('counterDP');
+let count = 0
+
+subBtn.addEventListener("click", function() {
+    count -= 1; 
+    counterDP.innerHTML = count;
+})
+
+resetBtn.addEventListener("click", function() {
+    count = 0;
+    counterDP.innerHTML = count;
+})
+
+addBtn.addEventListener("click", function() {
+    count += 1;
+    counterDP.innerHTML = count;
+})
 
